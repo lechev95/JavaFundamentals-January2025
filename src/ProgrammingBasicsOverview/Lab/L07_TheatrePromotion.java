62f5c20c3269e4eb6cbccf976b8b343920c9aff5
lechev95 <lecho.lechev@gmail.com>
2024-11-20 10:33:50 +0200

    Task 07 from Basic Syntax, Conditional Statements and Loops - Lab

--- src/ProgrammingBasicsOverview/Lab/L07_TheatrePromotion.java
+++ src/ProgrammingBasicsOverview/Lab/L07_TheatrePromotion.java
@@ -8,18 +8,38 @@ public class L07_TheatrePromotion {
 
         String day = input.nextLine();
         int age = Integer.parseInt(input.nextLine());
-        int price;
+        double price = 0.0;
 
         switch (day) {
             case "Weekday":
-                if (age >= 0 && age <= 18) {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
                     price = 12;
+                } else if (age > 18 && age <= 64) {
+                    price = 18;
                 }
                 break;
             case "Weekend":
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+                    price = 15;
+                } else if (age > 18 && age <= 64) {
+                    price = 20;
+                }
                 break;
             case "Holiday":
+                if (age >= 0 && age <= 18) {
+                    price = 5;
+                } else if (age > 18 && age <= 64) {
+                    price = 12;
+                } else if (age > 64 && age <= 122) {
+                    price = 10;
+                }
                 break;
+        }
+
+        if (age < 0) {
+            System.out.println("Error!");
+        } else {
+            System.out.printf("%.0f$", price);
         }
     }
 }
